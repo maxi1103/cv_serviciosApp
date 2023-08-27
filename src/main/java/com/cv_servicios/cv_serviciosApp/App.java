95b4aed5a76ace0a22f5c676b3c9e89c31a57dab
DESKTOP-9UJEHGC\maxi <nahuel_maxi11@hotmail.com>
2023-08-26 22:57:14 -0300

    1er commit, estrucutra del proyecto, loginUI

---
+++ src/main/java/com/cv_servicios/cv_serviciosApp/App.java
@@ -0,0 +1,13 @@
+package com.cv_servicios.cv_serviciosApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/cv_servicios/cv_serviciosApp/AppTest.java
@@ -0,0 +1,20 @@
+package com.cv_servicios.cv_serviciosApp;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
